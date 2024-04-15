d398950c9730f5f4b2beafc2eb31c21c0cc8b089
ninthu-hologo <ninthukesan@hologo.world>
2024-04-15 18:27:06 +0530

    update baseURL

--- src/utils/axiosPrivate.js
+++ src/utils/axiosPrivate.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
-// const BASE_URL = 'https://hologo-dash-api.onrender.com';
+const BASE_URL = 'https://hologo-dash-api.onrender.com';
 // const BASE_URL = 'http://localhost:3500'
 
-const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3500' : ''
+// const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3500' : ''
 
 console.log(process.env.NODE_ENV)
 
