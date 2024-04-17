b885e11cefce8713405bbb3d6144798dc6f9e60c
ninthu-hologo <ninthukesan@hologo.world>
2024-04-17 17:39:10 +0530

    final build

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 // const BASE_URL = 'https://hologo-dash-api.onrender.com';
-const BASE_URL = '/'
+const BASE_URL = '/api'
 
 export default axios.create({
     baseURL: BASE_URL
--- src/utils/axiosPrivate.js
+++ src/utils/axiosPrivate.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 // const BASE_URL = 'https://hologo-dash-api.onrender.com';
-const BASE_URL = '/'
+const BASE_URL = '/api'
 
 // const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3500' : ''
 
